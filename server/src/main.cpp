177a9469409ec54ad24ae3a13fcabb434a62f1a7
Alexandre Vigneau <alexandre.vigneau@epitech.eu>
2024-11-26 14:59:53 +0100

    [ADD]: Start of network library

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,7 +1,18 @@
 #include "spdlog/spdlog.h"
+#include "NetworkWrapper.hpp"
 
 int main(int argc, char const *argv[])
 {
-    spdlog::info("Start R-Type Server!");
+    network::NetworkServer server;
+
+    if (server.start(12345)) {
+        spdlog::info("Start R-Type Server!");
+        while (true) {
+            server.processEvents();
+        }
+    } else {
+        spdlog::error("Failed to start server.");
+    }
+
     return 0;
 }
