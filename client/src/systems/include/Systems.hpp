b742b8d0fed47b725bffbba9729c9084718c4707
lowan <lowan.ferraro@epitech.eu>
2024-12-02 21:45:42 +0100

    [ADD] Ecs add unsubscribe all

--- client/src/systems/include/Systems.hpp
+++ client/src/systems/include/Systems.hpp
@@ -7,9 +7,19 @@
 // Window
 void init_window_system(Registry& ecs, const WindowOpenEvent&);
 void close_window_system(Registry& ecs, const WindowCloseEvent&);
-void update_window_system(Registry& ecs, const WindowUpdateEvent&);
 
+// Menu
+void draw_menu_system(Registry& ecs, const WindowDrawEvent&);
+
+// Camera
 void create_camera_system(Registry& ecs, const InitCameraEvent&);
+
+// Models
 void load_models_system(Registry& ecs, const InitModelEvent&);
 void apply_shader_system(Registry& ecs, const InitModelEvent&);
 void unload_models_system(Registry& ecs, const WindowCloseEvent&);
+
+// Controls
+void selector_controls_system(Registry &e, const ControlsEvent&);
+void menu_controls_system(Registry &e, const ControlsEvent &);
+void game_controls_system(Registry &e, const ControlsEvent&);
