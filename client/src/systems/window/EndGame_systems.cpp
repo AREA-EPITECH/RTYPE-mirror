544083231c7905f2c8c266fa49f0eef906d9ff2f
Alexandre Vigneau <alexandre.vigneau@epitech.eu>
2025-01-17 16:07:21 +0100

    [UPD]: Start of game over screen

--- client/src/systems/window/EndGame_systems.cpp
+++ client/src/systems/window/EndGame_systems.cpp
@@ -16,6 +16,24 @@ namespace ecs {
     }
 
     void open_endgame_system(Registry &ecs, const WindowOpenEvent &) {
+        kill_entities_with_component<ecs::ShowBoxComponent>(ecs);
+        kill_entities_with_component<ecs::TextInputComponent>(ecs);
+        kill_entities_with_component<ecs::ButtonComponent>(ecs);
+        kill_entities_with_component<ecs::TextComponent>(ecs);
+        kill_entities_with_component<ecs::CloseLeaderBoard>(ecs);
+        kill_entities_with_component<ecs::VesselsComponent>(ecs);
+        kill_entities_with_component<ecs::ProjectilesComponent>(ecs);
+        kill_entities_with_component<ecs::ControllableComponent>(ecs);
+        kill_entities_with_component<ecs::MenuText>(ecs);
+        kill_entities_with_component<ecs::ShaderComponent>(ecs);
+        kill_entities_with_component<ecs::CameraComponent>(ecs);
+        kill_entities_with_component<ecs::LightComponent>(ecs);
+        kill_entities_with_component<ecs::BackgroundComponent>(ecs);
+        kill_entities_with_component<ecs::DecorElementComponent>(ecs);
+        kill_entities_with_component<ecs::HealthBarComponent>(ecs);
+        kill_entities_with_component<ecs::ParticleSystemComponent>(ecs);
+        kill_entities_with_component<ecs::ImageComponent>(ecs);
+        kill_entities_with_component<ecs::FilterComponent>(ecs);
     }
 
     void close_endgame_system(Registry &ecs, const WindowCloseEvent &) {
