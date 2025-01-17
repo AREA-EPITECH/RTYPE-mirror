491b1d6c79bf6cf7ade3ae3fa35b5842aed37853
lowan <lowan.ferraro@epitech.eu>
2025-01-17 16:13:15 +0100

    [ADD] add music in end_game

--- client/src/systems/window/EndGame_systems.cpp
+++ client/src/systems/window/EndGame_systems.cpp
@@ -9,6 +9,12 @@
 
 namespace ecs {
     void draw_endgame_system(Registry &ecs, const WindowDrawEvent &) {
+        auto &musics = ecs.get_components<ecs::MusicComponent>();
+        for (auto &music : musics) {
+            if (music.has_value()) {
+                music.value().update("win_music");
+            }
+        }
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), Fade(BLUE, 0.9f));
@@ -34,8 +40,24 @@ namespace ecs {
         kill_entities_with_component<ecs::ParticleSystemComponent>(ecs);
         kill_entities_with_component<ecs::ImageComponent>(ecs);
         kill_entities_with_component<ecs::FilterComponent>(ecs);
+
+        auto &musics = ecs.get_components<ecs::MusicComponent>();
+        for (auto &music : musics) {
+            if (music.has_value()) {
+                music.value().addMusic("win_music", "./client/assets/sound/win_music.wav");
+                music.value().play("win_music");
+                break;
+            }
+        }
     }
 
     void close_endgame_system(Registry &ecs, const WindowCloseEvent &) {
+        auto &musics = ecs.get_components<ecs::MusicComponent>();
+        for (auto &music : musics) {
+            if (music.has_value()) {
+                music.value().stop("win_music");
+                break;
+            }
+        }
     }
 }
